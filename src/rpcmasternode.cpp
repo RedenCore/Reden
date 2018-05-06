335cb4372b11a9caa1c2349871edc9929f701ebf
Tri <tri282@gmail.com>
2018-05-05 23:48:40 -0700

    testing

--- src/rpcmasternode.cpp
+++ src/rpcmasternode.cpp
@@ -114,6 +114,7 @@ UniValue generatemasternodecollateral(const UniValue& label, bool fHelp) {
 	UniValue newAddressParam(UniValue::VSTR, label.get_str());
 	// create new address for masternode
 	UniValue newAddress = getnewaddress(newAddressParam, fHelp);
+
 	std::cout << "new address ---" << newAddress.get_str().c_str();
 	UniValue sendParams(UniValue::VOBJ);
 	CAmount collateralAmount = 10 * COIN;
